8fc640a892d5e412a13319dd6ff982115d3ccab0
John Turner <johnturner4004@gmail.com>
2023-08-18 21:12:12 -0400

    feat: get game list from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function App (): JSX.Element {
+  const gameList = useSelector((store: any) => store.gameReducer)
   const dispatch = useDispatch()
+  console.log('gameList:', gameList)
 
-  dispatch({ type: 'FETCH_GAME' })
+  useEffect(() => {
+    dispatch({ type: 'FETCH_GAME' })
+  }, [])
 
   return (
     <h1>Hello from React!</h1>
